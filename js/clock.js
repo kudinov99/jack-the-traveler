adf1b48e5de3077499cb8e212454de897b1d0f56
kudinov99 <texhnolyzed.soul@gmail>
2020-11-28 17:40:03 +0200

    28.11.2020

--- js/clock.js
+++ js/clock.js
@@ -18,13 +18,3 @@
 //  }, 1000
 //  )
 
-function printElapsedTime(fTest) {
-    let nStartTime = Date.now(),
-        vReturn = fTest(),
-        nEndTime = Date.now()
-  
-    console.log(`Elapsed time: ${ String(nEndTime - nStartTime) } milliseconds`)
-    return vReturn
-  }
-  
-  let yourFunctionReturn = printElapsedTime(yourFunction)

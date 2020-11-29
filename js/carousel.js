6b269af77b115aa09db85c53f06cd4604e581871
kudinov99 <texhnolyzed.soul@gmail>
2020-11-29 21:00:40 +0200

    28.11.2020

--- js/carousel.js
+++ js/carousel.js
@@ -1,38 +1,4 @@
-const images = [
-    'img/carousel/backpacks-on-stones.jfif',
-    'img/carousel/mountains-big-river.jfif',
-    'img/carousel/mountains-river.jfif',
-    'img/carousel/mountains-sky.jfif',
-    'img/carousel/mountains-waterfall.jfif',
-    'img/carousel/taipai-station.jfif',
-    'img/carousel/travel-equipment.jfif',
-    'img/carousel/waterfalll.jfif'
-]
-let currentIndex = 0; 
-const interval = setInterval(slide, 1000);
-
-function slide() {
-    currentIndex =  ++currentIndex > images.length - 1 ? 0 : currentIndex;
-    showSlide(currentIndex);
-}
-function back() {
-    currentIndex =  --currentIndex < 0 ? images.length - 1 : currentIndex;
-    showSlide(currentIndex);
-}
-function forward() {
-    currentIndex =  ++currentIndex > images.length - 1 ? 0 : currentIndex;    
-    showSlide(currentIndex);
-}
-function stop() {
-    clearInterval(interval);
-}
-
-document.querySelector('.carousel .button-back').addEventListener('click', back)
-document.querySelector('.carousel .button-forward').addEventListener('click', forward)
-document.querySelector('.carousel .button-stop').addEventListener('click', stop)
-
-function showSlide(currentIndex) {
-    document.querySelector('.carousel img').src = images[currentIndex];
-}
-
-
+  .carousel-inner img {
+      width: 100%;
+      height: 100%;
+  }
